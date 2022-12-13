d31f3d68441f8910b685039da2ddda753b09cb8f
lucasporrini <2608lucas@gmail.com>
2022-12-14 00:05:19 +0100

    bug base de données corrigé ------- sauvegarde reference ------ controller ok, entity ok, repo ok

--- migrations/Version20221213230229.php
+++ migrations/Version20221213230229.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221213185609 extends AbstractMigration
+final class Version20221213230229 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20221213185609 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE annonce CHANGE created_ad created_at DATE DEFAULT NULL');
+        $this->addSql('ALTER TABLE categorie CHANGE title cat_title VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE annonce CHANGE created_at created_ad DATE DEFAULT NULL');
+        $this->addSql('ALTER TABLE categorie CHANGE cat_title title VARCHAR(255) DEFAULT NULL');
     }
 }
